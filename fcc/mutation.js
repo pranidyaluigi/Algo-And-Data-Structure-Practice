38736c2ae1e17e3c46ca8033ee28b3dcdab44299
Pranidya Luigi <pranidyaluigi@gmail.com>
2022-01-25 01:52:37 +0700

    mutation

--- fcc/mutation.js
+++ fcc/mutation.js
@@ -46,9 +46,9 @@ function mutation(arr) {
   //   return true;
   let count = 0;
 
-  for (let i = 0; i < str1.length; i++) {
-    for (let j = 0; j < str2.length; j++) {
-      if (str1[i] === str2[j]) {
+  for (let i = 0; i < str2.length; i++) {
+    for (let j = 0; j < str1.length; j++) {
+      if (str2[i] === str1[j]) {
         count++;
         break;
       }
